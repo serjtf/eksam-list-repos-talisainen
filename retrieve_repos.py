0d81f0bffa77a9fb397504b9de870c76aa1a421a
serjtf <107878358+serjtf@users.noreply.github.com>
2022-06-20 18:59:37 +0100

    Update retrieve_repos.py

--- retrieve_repos.py
+++ retrieve_repos.py
@@ -3,5 +3,5 @@ import requests, json
 username = "talisainen"
 def retrieve_repos(username):
     url = "https://api.github.com/users/" + username + "/repos"
-    content = json.loads(requests.get(url).text)
+    #content = json.loads(requests.get(url).text)
     return len(content)
